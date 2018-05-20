40cf51078aaab247d7bdb55f00f91526aca4c67a
leihua <leihua@masslottery.com>
2018-05-19 20:09:53 -0400

    menu

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React, { Component, Fragment } from 'react'
-import { BrowserRouter, Route } from 'react-router-dom'
+import { BrowserRouter, Route, Link } from 'react-router-dom'
 
 import Home from './routes/Home'
 import About from './routes/About'
@@ -10,6 +10,20 @@ class App extends Component {
     return (
       <BrowserRouter>
         <Fragment>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/user/Lei">User Lei</Link>
+            </li>
+            <li>
+              <Link to="/user/Kim">User Kim</Link>
+            </li>
+          </ul>
           <Route path="/" exact component={Home} />
           <Route path="/about" exact component={About} />
           <Route path="/user/:username" exact strict component={User} />
